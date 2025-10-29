9dcb7b04f360c02799fbfad4349f6b309fcac02f
Yosefinagst <yosefnaagustine@gmail.com>
2025-10-30 00:26:53 +0700

    testing

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -1,5 +1,7 @@
 <?php
 
+//gata ini coba aja buat testing masuk ke branch 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
